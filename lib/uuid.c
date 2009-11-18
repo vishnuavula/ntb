da74a27f963d0a888fa2f32ac673657ddc677cbd
Dan Williams <dan.j.williams@intel.com>
2009-11-18 16:08:10 -0700

    lib: add two routines for parsing and printing an 128-bit uuid
    A bbu block device cache tracks its backing device with a 128-bit uuid.
    parse_uuid() and print_uuid() allow the kernel to read and write these
    values across a sysfs interface.  This implementation was derived from
    mdadm which uses 128-bit uuid's to identify individual raid arrays.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- lib/uuid.c
+++ lib/uuid.c
@@ -51,3 +51,71 @@ void uuid_be_gen(uuid_be *bu)
 	bu->b[6] = (bu->b[6] & 0x0F) | 0x40;
 }
 EXPORT_SYMBOL_GPL(uuid_be_gen);
+
+char *uuid_to_string(char *buf, const char uuid[16], int sysfs)
+{
+	int i, j;
+	int id;
+	char *c = buf;
+
+	for (i = 0; i < 4; i++) {
+		id = uuid[i];
+		if (i)
+			*c++ = '-';
+		for (j = 3; j >= 0; j--) {
+			sprintf(c, "%02x", (unsigned char) uuid[j+4*i]);
+			c += 2;
+		}
+	}
+	if (sysfs)
+		*c++ = '\n';
+	*c = 0;
+
+	return buf;
+}
+EXPORT_SYMBOL(uuid_to_string);
+
+
+/* parse_uuid - parse a 128 bit uuid into 4 integers
+ * @uuid: integer array to store parsed value
+ * @str: text formatted uuid
+ *
+ * The format is 32 hex nibbles with optional :.<space>-\n separators.
+ * If the input is not exactly 32 hex digits, return -EINVAL else return 0.
+ */
+int parse_uuid(int uuid[4], const char *str)
+{
+	int hit = 0; /* number of hex digit */
+	int i;
+	char c;
+
+	for (i = 0; i < 4; i++)
+		uuid[i] = 0;
+
+	while ((c = *str++)) {
+		int n;
+
+		if (c >= '0' && c <= '9')
+			n = c-'0';
+		else if (c >= 'a' && c <= 'f')
+			n = 10 + c - 'a';
+		else if (c >= 'a' && c <= 'f')
+			n = 10 + c - 'a';
+		else if (strchr(":. -\n", c))
+			continue;
+		else
+			return -EINVAL;
+
+		if (hit < 32) {
+			uuid[hit / 8] <<= 4;
+			uuid[hit / 8] += n;
+		}
+		hit++;
+	}
+
+	if (hit == 32)
+		return 0;
+
+	return -EINVAL;
+}
+EXPORT_SYMBOL(parse_uuid);
