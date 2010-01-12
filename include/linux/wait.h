b6a0d3669267ee8d7aad69f7fe47f9a2373fa3f6
Dan Williams <dan.j.williams@intel.com>
2010-01-12 12:32:21 -0700

    make wake_event_lock_irq generically available
    The wake_event_lock_irq macro allows an event to be waited for where
    evaluation of the 'condition' must happen under spin_lock_irq.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- include/linux/wait.h
+++ include/linux/wait.h
@@ -396,6 +396,48 @@ do {									\
 	__ret;								\
 })
 
+#define __wait_event_lock_irq(wq, condition, lock, cmd) 		\
+do {									\
+	wait_queue_t __wait;						\
+	init_waitqueue_entry(&__wait, current);				\
+									\
+	add_wait_queue(&wq, &__wait);					\
+	for (;;) {							\
+		set_current_state(TASK_UNINTERRUPTIBLE);		\
+		if (condition)						\
+			break;						\
+		spin_unlock_irq(&lock);					\
+		cmd;							\
+		schedule();						\
+		spin_lock_irq(&lock);					\
+	}								\
+	current->state = TASK_RUNNING;					\
+	remove_wait_queue(&wq, &__wait);				\
+} while (0)
+
+/**
+ * wait_event_lock_irq - sleep until a condition, evaluated under lock, is true
+ * @wq: the waitqueue to wait on
+ * @condition: a C expression for the event to wait for
+ * @lock: spinlock_t to acquire before evaluating condition
+ * @cmd: optional statement to run after each false wakeup
+ *
+ * The process is put to sleep (TASK_UNINTERRUPTIBLE) until the
+ * @condition evaluates to true. The @condition is checked, under
+ * spin_lock_irq(&lock),  each time the waitqueue @wq is woken up.
+ *
+ * wake_up() must be called after changing any variable that could
+ * change the result of the wait condition.
+ */
+#define wait_event_lock_irq(wq, condition, lock, cmd) 			\
+do {									\
+	if (condition)	 						\
+		break;							\
+	__wait_event_lock_irq(wq, condition, lock, cmd);		\
+} while (0)
+
+
+
 /*
  * Must be called with the spinlock in the wait_queue_head_t held.
  */
